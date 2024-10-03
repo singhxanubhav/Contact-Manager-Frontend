4f4e4293fcae6c6856608130f49d8c9ba180dc0b
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-10-03 22:18:22 +0530

    Project Setup

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <Header />
+      <AddContact />
+      {/* <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
